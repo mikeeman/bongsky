0bd39922a62dea4da9c8b31bb0679a8e2a78b7a4
mikeeman <mikee.man@gmail.com>
2018-02-13 22:19:47 -0500

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#load env vars
+. ./../deploy.sh
+#kill old instance
+read -r processid < ./tmp/pids/server.pid
+kill processid
+#precompile assets
+rake assets:precompile RAILS_ENV=production
+#start server as a service running in background
+rails server -b 0.0.0.0 -p 3000 -e production -d
