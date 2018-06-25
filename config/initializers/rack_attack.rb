7ffd4c83b7f32b1ba3a3cf2026f30e4a1fd59c0a
mikeeman <mikee.man@gmail.com>
2018-06-25 08:48:13 -0400

    Implement throttling for 300 requests per 5 minutes for all ips

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -17,3 +17,32 @@ Rack::Attack.blocklist_ip("195.204.204.165")
 Rack::Attack.blocklist_ip("66.249.65.116")
 Rack::Attack.blocklist_ip("141.212.122.144")
 Rack::Attack.blocklist_ip("34.201.223.224")
+
+### Configure Cache ###
+
+# If you don't want to use Rails.cache (Rack::Attack's default), then
+# configure it here.
+#
+# Note: The store is only used for throttling (not blacklisting and
+# whitelisting). It must implement .increment and .write like
+# ActiveSupport::Cache::Store
+
+Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new 
+
+
+### Throttle Spammy Clients ###
+
+# If any single client IP is making tons of requests, then they're
+# probably malicious or a poorly-configured scraper. Either way, they
+# don't deserve to hog all of the app server's CPU. Cut them off!
+#
+# Note: If you're serving assets through rack, those requests may be
+# counted by rack-attack and this throttle may be activated too
+# quickly. If so, enable the condition to exclude them from tracking.
+
+# Throttle all requests by IP (60rpm)
+#
+# Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
+Rack::Attack.throttle('req/ip', limit: 300, period: 5.minutes) do |req|
+  req.ip # unless req.path.start_with?('/assets')
+end
