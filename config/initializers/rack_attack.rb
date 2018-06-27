762258dec9da13c8f59063422aa4c2a49151b3c1
mikeeman <mikee.man@gmail.com>
2018-06-26 20:13:40 -0400

    Block 2 more ips...

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -22,6 +22,8 @@ Rack::Attack.blocklist_ip("66.249.66.77")
 Rack::Attack.blocklist_ip("189.39.117.66")
 Rack::Attack.blocklist_ip("179.108.39.81")
 Rack::Attack.blocklist_ip("183.185.136.156")
+Rack::Attack.blocklist_ip("66.249.66.76")
+Rack::Attack.blocklist_ip("66.249.66.78")
 
 ### Configure Cache ###
 
