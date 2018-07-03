3a68631be8cea1ebf5e735c821335677397417a6
mikeeman <mikee.man@gmail.com>
2018-07-03 00:05:12 -0400

    Block an ip

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -51,6 +51,7 @@ Rack::Attack.blocklist_ip("5.255.250.85")
 Rack::Attack.blocklist_ip("139.219.100.104")
 Rack::Attack.blocklist_ip("212.237.46.77")
 Rack::Attack.blocklist_ip("89.120.213.253")
+Rack::Attack.blocklist_ip("101.200.190.141")
 
 ### Configure Cache ###
 
